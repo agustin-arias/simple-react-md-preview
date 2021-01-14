20348cdea8a2fd322597f47c0080d852064f7139
agustinntarias <gustin444@gmail.com>
2021-01-13 21:42:55 -0300

    Minor changes

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,5 +1,4 @@
-import { faExpandArrowsAlt } from "@fortawesome/free-solid-svg-icons";
-import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faCode, faExpandArrowsAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
@@ -9,7 +8,10 @@ interface ToolbarProps {
 
 export const Toolbar: React.FC<ToolbarProps> = ({ name }) => {
   return (
-    <div className="d-flex justify-content-between border-bottom border-dark px-2">
+    <div
+      className="d-flex justify-content-between border-bottom border-dark px-2"
+      key={`toolbar-${name}`}
+    >
       <div className="d-flex justify-content-between">
         <FontAwesomeIcon icon={faCode} style={{ marginTop: "3px" }} />
         <div className="ml-2 font-weight-bold text-capitalize">{name}</div>
