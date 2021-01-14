30ceef34d6704add50d56da4657a56448df81b64
agustinntarias <gustin444@gmail.com>
2021-01-13 21:42:46 -0300

    Added Previewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Editor } from "./components/Editor";
+import { Previewer } from "./components/Previewer";
 
 function App() {
-  return <Editor />;
+  const [code, setCode] = useState("asdasdsa");
+
+  return (
+    <>
+      <Editor
+        code={code}
+        handleChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+          setCode(event.target.value);
+        }}
+      />
+      <Previewer code={code} />
+    </>
+  );
 }
 
 export default App;
