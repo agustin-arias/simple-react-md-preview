e00f1137b41829e3994c5f99cab71be68a011b7b
agustinntarias <gustin444@gmail.com>
2021-01-13 21:41:49 -0300

    Added onChange and value attributes

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-interface TextAreaProps {}
+interface TextAreaProps {
+  text: string;
+  handleChange:
+    | ((event: React.ChangeEvent<HTMLTextAreaElement>) => void)
+    | undefined;
+}
 
-export const TextArea: React.FC<TextAreaProps> = () => {
+export const TextArea: React.FC<TextAreaProps> = ({ text, handleChange }) => {
   return (
     <textarea
       className="form-control rounded-0 p-1 m-0"
-      id="exampleFormControlTextarea1"
+      id="editor"
+      value={text}
+      onChange={handleChange}
       style={{ height: "200px", fontSize: ".9em" }}
     ></textarea>
   );
